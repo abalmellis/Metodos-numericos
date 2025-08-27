09535df4caff15be5729fb6da5ab62ed29e2e928
abalmellis <abalmellis@gmail.com>
2025-08-27 19:26:23 -0400

    Add files via upload

---
+++ Tarea1.py
@@ -0,0 +1,17 @@
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+
+with h5py.File('datasets.hd5', 'r') as file:
+    x = np.array(file['x'])
+    y = np.array(file['y'])
+    e = np.array(file['e'])
+
+plt.errorbar(x, y, yerr=e, label='Datos')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Gráfico con barras de error')
+plt.legend()
+plt.grid(True)
+plt.show()
+
